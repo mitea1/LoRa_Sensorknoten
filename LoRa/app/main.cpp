69f7c79c9c9cb4c2a1abeb8568e54f0b95515829
Adrian Mitevski <adrian.mitevski@students.bfh.ch>
2016-06-07 09:26:51 +0200

    Application Configs added and prepared for Hardware Tests

--- LoRa/app/main.cpp
+++ LoRa/app/main.cpp
@@ -1,26 +1,3 @@
-#include "mbed.h"
-#include "mDot.h"
-#include "MTSLog.h"
-#include "uBlox.h"
-#include "I2C_RT.h"
-#include "MAX44009.h"
-#include "BME280.h"
-#include "MPU9250.h"
-#include "SI1143.h"
-#include "LoRa.h"
-#include "TaskLight.h"
-#include "TaskTemperature.h"
-#include "TaskHumidity.h"
-#include "TaskPressure.h"
-#include "TaskAcceleration.h"
-#include "TaskGyroscope.h"
-#include "TaskTesla.h"
-#include "TaskGPS.h"
-#include "TaskDatahandler.h"
-#include "main.h"
-#include <string>
-#include <vector>
-#include <algorithm>
 #include "SensorHandler.h"
 #include "ApplicationConfig.h"
 
@@ -28,7 +5,7 @@ int main() {
 
 
 	SensorHandler sensorHandler;
-	sensorHandler.init(APPLICATION_MODE_1);
+	sensorHandler.init(APPLICATION_MODE_TEST_MAX44009);
     while (true) {
 
     }
